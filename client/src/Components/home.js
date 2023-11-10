5c599ae91e46da1f80b08e8c5f67cc889401f59e
John Marcos <johnsaguay@gmail.com>
2023-11-09 23:45:17 -0500

    Created home compoonent and server folder

---
+++ client/src/Components/home.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Home() {
+    return(
+        <div className="row">
+            <div className="col-6">
+                <p >Hello, Welcome PM1, here to make your job easier</p>
+            </div>
+            <div className="col-6">
+                <p >Any service orders pending?</p>
+            </div>
+            <div className="col-3">
+                <p >Who's moving out?</p>
+            </div>
+        </div>
+    )
+}
